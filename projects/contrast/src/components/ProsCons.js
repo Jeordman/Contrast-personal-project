7751df23bbd8cbec0248b3c7f7dd12657fe87a62
Jeordin Callister <jeordman@Jeordins-MacBook-Air.local>
2019-06-28 18:38:16 -0600

    leaving

--- projects/contrast/src/components/ProsCons.js
+++ projects/contrast/src/components/ProsCons.js
@@ -7,7 +7,7 @@ class ProsCons extends Component {
     super();
 
     this.state = {
-      fakeVariable: 0
+      
     };
   }
 
@@ -16,46 +16,52 @@ class ProsCons extends Component {
     console.log("this is in proscons", currentItemLeft, currentItemRight);
     return (
       <article classname="full-pros-cons">
-      
         <section className="left-pros-cons">
-
-        
-         
           <section className="left-pros">
             {" "}
             {currentItemLeft.pros &&
               currentItemLeft.pros.map(obj => {
-                return <li className='all-lists'>{obj.description}</li>;
+                return <li className="all-lists">{obj.description}</li>;
               })}
           </section>
           <section className="left-cons">
             {currentItemLeft.cons &&
               currentItemLeft.cons.map(obj => {
-                return <li className='all-lists'>{obj.description}</li>;
+                return <li className="all-lists">{obj.description}</li>;
               })}
           </section>
         </section>
 
-
-          <section className="right-pros-cons">
-            <ul className="right-pros">
+        <section className="right-pros-cons">
+          <ul className="right-pros">
             {" "}
             {currentItemRight.pros &&
               currentItemRight.pros.map(obj => {
-                return <li className='all-lists'>{obj.description}</li>;
+                return <li className="all-lists">{obj.description}</li>;
               })}
-            </ul>
-            <ul className="right-cons">
+          </ul>
+          <ul className="right-cons">
             {currentItemRight.cons &&
               currentItemRight.cons.map(obj => {
-                return <li className='all-lists'>{obj.description}</li>;
+                return <li className="all-lists">{obj.description}</li>;
               })}
-            </ul>
-         </section>
+          </ul>
+        </section>
 
-         <footer className='left-foot' > EY</footer>
-         <footer className='right-foot'> EY</footer>
+        <div className="left-foot">
+          <input placeholder="...pro" className='add-pro-input-left'/> 
+          <button className='add-pro-left-button' > Add</button>
 
+          <input placeholder="...pro" className='add-pro-input-right'/> 
+          <button className='add-pro-right-button' > Add</button>
+        </div>
+        <div className="right-foot">
+        <input placeholder="...pro" className='add-pro-input-left-2'/> 
+          <button className='add-pro-left-button-2' > Add</button>
+
+          <input placeholder="...pro" className='add-pro-input-right-2'/> 
+          <button className='add-pro-right-button-2' > Add</button>
+        </div>
       </article>
     );
   }
